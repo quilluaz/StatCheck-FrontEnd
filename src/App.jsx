3646842c3ef18687bba1374bb3d037133c5a7b63
Isaac <quiloisaac@gmail.com>
2024-11-15 05:07:44 +0800

    Updated Admin Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import UserRoutes from "./routes/UserRoutes";
+// import AdminRoutes from "./routes/AdminRoutes";
 
 const App = () => {
   return (
     <Router>
       <UserRoutes />
+      {/* <AdminRoutes /> */}
     </Router>
   );
 };
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "../userPages/Landing";
 import Home from "../userPages/Home";
 // import Buildings from "../userPages/Buildings";
- import BuildingsRTL from "../userPages/Buildings-RTL";
+import BuildingsRTL from "../userPages/Buildings-RTL";
 import BuildingsNGE from "../userPages/Buildings-NGE";
- import BuildingsGLE from "../userPages/Buildings-GLE";
+import BuildingsGLE from "../userPages/Buildings-GLE";
 // import Reservation from "../userPages/Reservation";
 import ReservationLibraryRoom from "../userPages/Reservation-LibraryRoom";
 import ReservationParkingSlot from "../userPages/Reservation-ParkingSlot";
@@ -21,11 +21,17 @@ function UserRoutes() {
         {/* <Route path="/buildings" element={<Buildings />} /> */}
         <Route path="/buildings/rtl" element={<BuildingsRTL />} />
         <Route path="/buildings/nge" element={<BuildingsNGE />} />
-        { <Route path="/buildings/gle" element={<BuildingsGLE />} /> }
+        {<Route path="/buildings/gle" element={<BuildingsGLE />} />}
         {/* <Route path="/reservations" element={<Reservation />} /> */}
-        <Route path="/reservations/library" element={<ReservationLibraryRoom />} />
-        <Route path="/reservations/parking" element={<ReservationParkingSlot />} />
-         <Route path="/user-profile" element={<UserProfile />} /> 
+        <Route
+          path="/reservations/library"
+          element={<ReservationLibraryRoom />}
+        />
+        <Route
+          path="/reservations/parking"
+          element={<ReservationParkingSlot />}
+        />
+        <Route path="/user-profile" element={<UserProfile />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </>
